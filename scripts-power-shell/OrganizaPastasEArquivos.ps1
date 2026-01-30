5c2a74e769bc83ead21cc768fd695be5947fffb6
Henrique Pacheco <henriquepachecjose@gmail.com>
2026-01-30 20:59:17 -0300

    Delete scripts-power-shell directory

--- scripts-power-shell/OrganizaPastasEArquivos.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Para organizar pasta específica:
-$SourcePath = "C:\MinhaPasta"
-
-# Para organizar apenas tipos específicos:
-$Files = Get-ChildItem -Path $SourcePath -File -Include *.pdf, *.docx
